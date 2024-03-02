1b63182cf76efae671659ea7a019e89d6533bc0b
4efk <65010619+4efk@users.noreply.github.com>
2024-03-02 11:30:03 +0100

    added a test helicopter body

--- src/Helicopter/helicopter_controller.gd
+++ src/Helicopter/helicopter_controller.gd
@@ -1,7 +1,16 @@
 extends RigidBody3D
 
+var main_rotor_rpm = 0.0
+
+@onready var main_rotor_pos = $MainRotorPos.position
+@onready var tail_rotor_pos = $TailRotorPos.position
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		apply_force(Vector3(0, 5000, 0), main_rotor_pos)

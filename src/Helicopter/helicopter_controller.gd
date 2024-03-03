2dda0a1205824efddcd986574f0a013c3d2891cd
4efk <65010619+4efk@users.noreply.github.com>
2024-03-03 14:29:01 +0100

    main rotor physics work 1

--- src/Helicopter/helicopter_controller.gd
+++ src/Helicopter/helicopter_controller.gd
@@ -1,16 +1,25 @@
 extends RigidBody3D
 
-var main_rotor_rpm = 0.0
+@export var coefficient_lift = 0.0
+@export var main_rotor_blades_n = 2
+
+var main_rotor_omega = 55.50 #angular velocity [rad/s]
 
 @onready var main_rotor_pos = $MainRotorPos.position
 @onready var tail_rotor_pos = $TailRotorPos.position
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("start_engine"):
+		main_rotor_omega = 55.50
+	if Input.is_action_just_pressed("stop_engine"):
+		main_rotor_omega = 0
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		apply_force(Vector3(0, 5000, 0), main_rotor_pos)
+	coefficient_lift = 1.225 * 10 * 10
+	
+	apply_central_force(transform.basis.y.round().normalized() * 0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n)#, main_rotor_pos)
+		
+	print(transform.basis.y.round() * 0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n)

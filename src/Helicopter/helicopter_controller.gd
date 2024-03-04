12f1b97bd872b46f9158d8bbc6249edfd3258d6a
4efk <65010619+4efk@users.noreply.github.com>
2024-03-04 10:32:15 +0100

    main rotor physics work 3

--- src/Helicopter/helicopter_controller.gd
+++ src/Helicopter/helicopter_controller.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @export var coefficient_lift = 0.0
 @export var main_rotor_blades_n = 2
+@export var main_rotor_area = 46.20 #main rotor disc area in m^2
 
 @onready var main_rotor_pos = $MainRotorPos.position
 @onready var tail_rotor_pos = $TailRotorPos.position
@@ -18,8 +19,9 @@ func _process(delta):
 		main_rotor_omega = 0
 	
 func _physics_process(delta):
-	coefficient_lift = 1.225 * 10 * 10
+	coefficient_lift = 1.225 * main_rotor_area * 0.1
 	
-	apply_force(Vector3(0, 1, 0) * 0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n, main_rotor_pos)
+	apply_force(transform.basis.y * 0.5 * main_rotor_omega * main_rotor_omega * coefficient_lift * main_rotor_blades_n, main_rotor_pos)
 		
-	print(Vector3(-0.5, 0.5, 0) * 0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n)# * (0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n))
+	#print(Vector3(-0.5, 0.5, 0) * 0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n)# * (0.5 * main_rotor_omega * coefficient_lift * main_rotor_blades_n))
+	print(linear_velocity)

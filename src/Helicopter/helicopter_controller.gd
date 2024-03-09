667a676b40fa30f723b4561ecfca9657f0bbf6a4
4efk <65010619+4efk@users.noreply.github.com>
2024-03-09 03:28:55 +0100

    tail rotor work

--- src/Helicopter/helicopter_controller.gd
+++ src/Helicopter/helicopter_controller.gd
@@ -38,8 +38,14 @@ func _physics_process(_delta):
 	
 	var main_rotor_thrust_direction = transform.basis.y.rotated(Vector3(1, 0, 0), cyclic.y * PI/12).rotated(Vector3(0, 0, 1), cyclic.x * PI/12)
 	
-	#apply_central_force(main_rotor_thrust_direction * main_rotor_thrust_force)
-	apply_force(main_rotor_thrust_direction * main_rotor_thrust_force, main_rotor_pos)
+	apply_central_force(main_rotor_thrust_direction * main_rotor_thrust_force)
+	#apply_force(main_rotor_thrust_direction * main_rotor_thrust_force, main_rotor_pos)
 	print(main_rotor_thrust_direction)
+	apply_torque(Vector3(0, -256.45 , 0))
+	
+	var tail_rotor_thrust_force = 64.47 + Input.get_axis("antitorque_right", "antitorque_left") * 400#2523.46
+	var tail_rotor_thrust_direction = Vector3(0, 0, 1)
+	
+	apply_force(tail_rotor_thrust_direction * tail_rotor_thrust_force, tail_rotor_pos)
 	
 	#print(linear_velocity)

b51e4dcc792fade42322b0dad48f7ffa10cb338a
4efk <65010619+4efk@users.noreply.github.com>
2024-03-11 17:05:45 +0100

    main rotor work

--- src/Helicopter/helicopter_controller.gd
+++ src/Helicopter/helicopter_controller.gd
@@ -8,7 +8,7 @@ extends RigidBody3D
 @export var main_rotor_collective_max = 12 # max/min angle of main rotor blades [°]; made up
 @export var main_rotor_collective_min = -12
 
-@onready var main_rotor_pos = $MainRotorPos.position
+@onready var main_rotor_pos = $MainRotorPos.global_position
 @onready var tail_rotor_pos = $TailRotorPos.position
 
 var main_rotor_omega =  55.50 # angular velocity [rad/s]
@@ -36,18 +36,27 @@ func _physics_process(_delta):
 	
 	cyclic = Input.get_vector("cyclic_forward", "cyclic_backward", "cyclic_left", "cyclic_right")
 	
-	print(cyclic)
+	#print(cyclic)
 	
-	var main_rotor_thrust_direction = transform.basis.y.rotated(Vector3(1, 0, 0), cyclic.y * PI/12).rotated(Vector3(0, 0, 1), cyclic.x * PI/12)
+	var main_rotor_thrust_direction = transform.basis.y.rotated(Vector3(1, 0, 0), cyclic.y * PI/30).rotated(Vector3(0, 0, 1), cyclic.x * PI/30)
+	main_rotor_thrust_direction = Vector3.UP.rotated(Vector3(1, 0, 0), cyclic.y * PI/30).rotated(Vector3(0, 0, 1), cyclic.x * PI/30)
 	
-	apply_central_force(main_rotor_thrust_direction * main_rotor_thrust_force)
+	print(main_rotor_thrust_direction)
+	
+	#apply_central_force(Vector3(0, 1, 0) * main_rotor_thrust_force)
+	#apply_central_force(main_rotor_thrust_direction * main_rotor_thrust_force)
 	#apply_force(main_rotor_thrust_direction * main_rotor_thrust_force, main_rotor_pos)
+	
+	apply_force(Vector3(0, 500 * 9.8, 0), main_rotor_pos)
+	
+	#if Input.is_action_pressed("start_engine"):
+		#apply_force(main_rotor_thrust_direction * main_rotor_thrust_force, main_rotor_pos)
 	
 	#print(main_rotor_thrust_direction)
 	#apply_torque(Vector3(0, -256.45 , 0))
 	
 	var tail_rotor_thrust_force = 64.47 + Input.get_axis("antitorque_right", "antitorque_left") * 400#2523.46
-	var tail_rotor_thrust_direction = Vector3(0, 0, 1)
+	var tail_rotor_thrust_direction = transform.basis.z
 	
 	#apply_force(tail_rotor_thrust_direction * tail_rotor_thrust_force, tail_rotor_pos)
 	

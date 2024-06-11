a93ace90d5831a11da6c49ed8f9b543ca48d2b96
4efk <65010619+4efk@users.noreply.github.com>
2024-06-11 23:49:42 +0200

    gameplay testing

--- src/MainMenu/main_menu.gd
+++ src/MainMenu/main_menu.gd
@@ -4,4 +4,4 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://World/world_0.tscn")
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://World/world_1.tscn")
+	get_tree().change_scene_to_file("res://World/flight_school.tscn")

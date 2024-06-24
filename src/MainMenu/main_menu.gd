a21a5a343d730c0ab57599cb86f06d5f74ea283e
4efk <65010619+4efk@users.noreply.github.com>
2024-06-24 12:55:44 +0200

    main menu work

--- src/MainMenu/main_menu.gd
+++ src/MainMenu/main_menu.gd
@@ -1,8 +1,21 @@
-extends CanvasLayer
+extends Node3D
+
+@onready var moving_main_rotor_part = $World/Helicopter/helicopter1_model_2/MovingMainRotorPart
+@onready var moving_tail_rotor_part = $World/Helicopter/helicopter1_model_2/MovingTailRotorPart
+
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var settings = $CanvasLayer/Settings
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	GlobalScript.flightschool_checkpoint = GlobalScript.DEFAULT_FLIGHTSCHOOL_CHECKPOINT.duplicate()
+
+func _process(delta):
+	var main_rotor_omega = 2
+	var tail_rotor_omega = 355.62828798/55.5 * main_rotor_omega
+	
+	moving_main_rotor_part.quaternion *= Quaternion(Vector3(0.0471064507, 0.99888987496, 0), main_rotor_omega * delta)
+	moving_tail_rotor_part.quaternion *= Quaternion(Vector3(0, 0, 1), tail_rotor_omega * delta)
 
 func _on_button_pressed():
 	GlobalScript.current_gamemode = 1
@@ -11,3 +24,7 @@ func _on_button_pressed():
 func _on_button_2_pressed():
 	GlobalScript.current_gamemode = 0
 	get_tree().change_scene_to_file("res://World/flight_school.tscn")
+
+func _on_settings_button_pressed():
+	main_menu.hide()
+	settings.show()
